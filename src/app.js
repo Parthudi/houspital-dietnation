8a9374a9ba3cfea36a7e0db5c1607078c7e1b6e6
Parthudi <65971741+Parthudi@users.noreply.github.com>
2020-11-29 16:00:24 +0530

    backend is ready

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 require('../src/db/mongoose')
 const router = require('./routes/userroutes')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 
+app.use(bodyParser.json())
 app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin' , '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization' );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+    next();
+})
 app.use(router)
-app.use(cors({origin:'http://localhost:3000'}))
+
 
 const port = 4000
 
